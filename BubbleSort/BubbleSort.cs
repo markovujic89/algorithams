d1f393791b368ae4b11d95d26c1e172cc887683a
mv89 <markoczvvujic@hotmail.com>
2023-05-09 15:57:30 +0200

    add bubble sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class BubbleSort
+    {
+        public int[] BubbleSortArray(int[] array)
+        {
+            bool noSwaps = false;
+
+            for(int i= array.Length; i>0; i--)
+            {
+                noSwaps = true;
+                for(int j=0; j< i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        noSwaps = false;
+                    }
+                }
+
+                if(noSwaps)
+                {
+                    break;
+                }
+            }
+            return array;
+        }
+    }
+}
+
+// [ 5, 7, 11, 10, 2, 15, 8]
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bubbleSort = new BubbleSort();
+
+            var sortedArray = bubbleSort.BubbleSortArray(new int[] { 5, 7, 11, 10, 2, 15, 8 });
+
+            foreach ( var item in sortedArray )
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
